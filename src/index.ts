5c20c8e9f2668a8ab4feb50b5c275eec98cd178e
Praveen Kumar <praveen@textmercato.com>
2022-02-07 10:58:35 +0530

    server instance variable name changed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Get, Post, Put, Delete, RestController, Provider, Inject } from './http/decorators';
-export { httpServer } from './http/server';
+export { httpServer as chocoServer } from './http/server';
 export { ChocoRequest, ChocoResponse } from './http/models';
